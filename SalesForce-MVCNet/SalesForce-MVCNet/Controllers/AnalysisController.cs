a1fa7037a5d795bcd0ffcc55bdb6afde77d321da
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-07-29 14:05:28 -0500

    Working on Filters

--- SalesForce-MVCNet/SalesForce-MVCNet/Controllers/AnalysisController.cs
+++ SalesForce-MVCNet/SalesForce-MVCNet/Controllers/AnalysisController.cs
@@ -1,17 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using SalesForce_MVCNet.Models;
 
 namespace SalesForce_MVCNet.Controllers
 {
     public class AnalysisController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private SortToken sortToken = new SortToken();
+        private System.Linq.IQueryable<SalesForce_MVCNet.Models.SurveyRecord> records;
+
+        public AnalysisController()
+        {
+            this.records = from rec in db.SurveyRecords
+                           select rec;
+        }
+
+
         // GET: Analysis
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        // GET: Filter
+        public ActionResult Filter(string sortOrder)
+        {
+            // Preset Ascendings/Decendings on first open
+            if (String.IsNullOrEmpty(sortOrder))
+            {
+                ViewBag.AgeSortParm = "childAge_asc";
+                ViewBag.CountrySortParam = "country_asc";
+                ViewBag.StateSortParam = "state_asc";
+                ViewBag.CitySortParam = "city_asc";
+                ViewBag.ResponseSortParam = "response_asc";
+            }
+            
+            // Otherwise Update existing sort orders
+            ViewBag.AgeSortParm = sortOrder == "childAge_asc" ? "childAge_desc" : ViewBag.AgeSortParm;
+            ViewBag.AgeSortParm = sortOrder == "childAge_desc" ? "childAge_asc" : ViewBag.AgeSortParm;
+            ViewBag.CountrySortParam = sortOrder == "country_asc" ? "country_desc" : ViewBag.CountrySortParam;
+            ViewBag.CountrySortParam = sortOrder == "country_desc" ? "country_asc" : ViewBag.CountrySortParam;
+            ViewBag.StateSortParam = sortOrder == "state_asc" ? "state_desc" : ViewBag.StateSortParam;
+            ViewBag.StateSortParam = sortOrder == "state_desc" ? "state_asc" : ViewBag.StateSortParam;
+            ViewBag.CitySortParam = sortOrder == "city_asc" ? "city_desc" : ViewBag.CitySortParam;
+            ViewBag.CitySortParam = sortOrder == "city_desc" ? "city_asc" : ViewBag.CitySortParam;
+            ViewBag.ResponseSortParam = sortOrder == "response_asc" ? "response_desc" : ViewBag.ResponseSortParam;
+            ViewBag.ResponseSortParam = sortOrder == "response_desc" ? "response_asc" : ViewBag.ResponseSortParam;
+
+            //var records = from rec in db.SurveyRecords
+            //                select rec;
+            switch (sortOrder)
+            {
+                case "childAge_desc":
+                    records = records.OrderBy(rec => rec.childAge);
+                    break;
+                case "childAge_asc":
+                    records = records.OrderByDescending(rec => rec.childAge);
+                    break;
+                case "country_desc":
+                    records = records.OrderBy(rec => rec.Country);
+                    break;
+                case "country_asc":
+                    records = records.OrderByDescending(rec => rec.Country);
+                    break;
+                case "state_desc":
+                    records = records.OrderBy(rec => rec.State);
+                    break;
+                case "state_asc":
+                    records = records.OrderByDescending(rec => rec.State);
+                    break;
+                case "city_desc":
+                    records = records.OrderBy(rec => rec.City);
+                    break;
+                case "city_asc":
+                    records = records.OrderByDescending(rec => rec.City);
+                    break;
+                case "response_desc":
+                    records = records.OrderBy(rec => rec.SurveyResponses);
+                    break;
+                case "response_asc":
+                    records = records.OrderByDescending(rec => rec.SurveyResponses);
+                    break;
+
+                default:
+                    records = records.OrderBy(rec => rec.childAge);
+                    break;
+            }
+
+            return View(records);
         }
     }
 }
---
+++ SalesForce-MVCNet/SalesForce-MVCNet/Models/SortToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesForce_MVCNet.Models
+{
+    public class SortToken
+    {
+        public bool childAge { get; set; }
+        public bool Country { get; set; }
+        public bool State { get; set; }
+        public bool City { get; set; }
+        public bool SurveyResults { get; set; }
+        public List<int> AgeSelection { get; set; }
+        public List<string> CountrySelection { get; set; }
+        public List<string> StateSelection { get; set; }
+        public List<string> CitySelection { get; set; }
+        public List<string> ResultsSelection { get; set; }
+    }
+}
